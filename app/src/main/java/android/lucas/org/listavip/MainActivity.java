75a5dfa30ec706a874e42bc57c7b2e135bdf91b7
lucasdinizv1 <lucasdinizvic@gmail.com>
2015-12-07 04:18:33 -0300

    tela de login criada com checkbox de mostrar senha

--- app/src/main/java/android/lucas/org/listavip/MainActivity.java
+++ app/src/main/java/android/lucas/org/listavip/MainActivity.java
@@ -1,37 +1,59 @@
 package android.lucas.org.listavip;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText etPassword, etUsername;
+    CheckBox checkSenha;
+    Button btLogin;
+    TextView tvCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    //teste
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        checkSenha = (CheckBox) findViewById(R.id.checkSenha);
+        tvCadastrar = (TextView) findViewById(R.id.tvCadastrar);
+        btLogin = (Button) findViewById(R.id.btLogin);
+
+        btLogin.setOnClickListener(this);
+        tvCadastrar.setOnClickListener(this);
+        checkSenha.setOnClickListener(this);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onClick(View v) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (v.getId()){
+            case R.id.btLogin:
+                break;
+
+            case R.id.tvCadastrar:
+                startActivity(new Intent(this, TelaCadastro.class));
+                break;
+
+            case R.id.checkSenha:
+                if (checkSenha.isChecked()) {
+                    etPassword.setTransformationMethod(null);
+                } else {
+                    etPassword.setTransformationMethod(new PasswordTransformationMethod());
+                }
+                break;
         }
 
-        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/android/lucas/org/listavip/TelaCadastro.java
@@ -0,0 +1,16 @@
+package android.lucas.org.listavip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class TelaCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_cadastro);
+    }
+
+}
